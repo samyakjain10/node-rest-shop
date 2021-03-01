3e04b4606c27576a0e874ffa8f06ae709d36f6e3
Samyak Jain <sjsamyak2001@gmail.com>
2021-03-01 11:32:20 +0530

    Handling errors and logs

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//If no route found
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+//Handle errors other than not found
+//Handle db errors etc
+app.use((error, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
