90275dc7e0c5c05eadc275e0ef45788d6cc17d8e
Samyak Jain <sjsamyak2001@gmail.com>
2021-03-02 16:56:19 +0530

    Adding Login

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,41 +2,42 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
-router.post('/signup', (req,res,next) => {
-    User.find({ email: req.body.email })
+router.post("/signup", (req, res, next) => {
+  User.find({ email: req.body.email })
     .exec()
-    .then(user => {
+    .then((user) => {
       if (user.length >= 1) {
         return res.status(409).json({
-          message: "Mail exists"
+          message: "Mail exists",
         });
       } else {
         bcrypt.hash(req.body.password, 10, (err, hash) => {
           if (err) {
             return res.status(500).json({
-              error: err
+              error: err,
             });
           } else {
             const user = new User({
               _id: new mongoose.Types.ObjectId(),
               email: req.body.email,
-              password: hash
+              password: hash,
             });
             user
               .save()
-              .then(result => {
+              .then((result) => {
                 console.log(result);
                 res.status(201).json({
-                  message: "User created"
+                  message: "User created",
                 });
               })
-              .catch(err => {
+              .catch((err) => {
                 console.log(err);
                 res.status(500).json({
-                  error: err
+                  error: err,
                 });
               });
           }
@@ -45,20 +46,64 @@ router.post('/signup', (req,res,next) => {
     });
 });
 
-router.delete("/:userId", (req, res, next) => {
-    User.findOneAndDelete(req.params.userId)
-      .exec()
-      .then(result => {
-        res.status(200).json({
-          message: "User deleted"
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed",
         });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed",
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed",
         });
       });
-  });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.delete("/:userId", (req, res, next) => {
+  User.findOneAndDelete(req.params.userId)
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "User deleted",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
 module.exports = router;
